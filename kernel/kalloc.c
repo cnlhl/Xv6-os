ff2c3c5fcb2d05eb2a30cac2a1a72258760f4245
Li Haolin <20307130260@fudan.edu.cn>
2022-12-20 20:05:36 +0800

    create count group for page

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -9,6 +9,8 @@
 #include "riscv.h"
 #include "defs.h"
 
+uint page_ref[(PHYSTOP - KERNBASE) / PGSIZE];
+
 void freerange(void *pa_start, void *pa_end);
 
 extern char end[]; // first address after kernel.
@@ -51,6 +53,12 @@ kfree(void *pa)
   if(((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
     panic("kfree");
 
+  if(page_ref[COW_INDEX(pa)] > 1) {
+    page_ref[COW_INDEX(pa)]--;
+    //release(&ref_lock);
+    return;
+  }
+  page_ref[COW_INDEX(pa)] = 0;
   // Fill with junk to catch dangling refs.
   memset(pa, 1, PGSIZE);
 
@@ -76,7 +84,9 @@ kalloc(void)
     kmem.freelist = r->next;
   release(&kmem.lock);
 
-  if(r)
+  if(r) {
     memset((char*)r, 5, PGSIZE); // fill with junk
+    page_ref[COW_INDEX(r)] = 1;
+  }
   return (void*)r;
 }
